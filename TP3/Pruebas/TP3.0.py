e089d5b66d6fa374e94c8891c2f894a7bf14e660
Alejandro Jazmatie <alejandro.jaz92@gmail.com>
2020-07-28 17:53:40 -0300

    finalizando tp 3

--- TP3/Pruebas/TP3.0.py
+++ TP3/Pruebas/TP3.0.py
@@ -1,229 +1,99 @@
-import math
-import os
-import sys
-import random
+import numpy as np
+from array import array
+import matplotlib.pyplot as pyplot
 
-# define las variables globales
-Q_LIMIT = 100
-BUSY = 1
-IDLE = 0
+class Colas(object):
 
-next_event_type = 0
-num_custs_delayed = 0
-num_delays_required = 0
-num_events = 0
-num_in_q = 0
-server_status = 0
-area_num_in_q = 0.0
-area_server_status = 0.0
-mean_interarrival = 0.0
-mean_service = 0.0
-time = 0.0
-time_arrival = [0]*(Q_LIMIT + 1)
-time_last_event = []
-time_next_event = [0]*3
-total_of_delays = 0.0
+    def __init__(self):
+        self.Reloj = 0.0
+        self.ListaDeEventos = array('f')
+        self.Paso = 0
+        self.Iniciado = False
+        self.TIempoUltimoEvento = 0.0
+        self.ProximoEvento = ""
+        self.EstadoServidor = ""
+        self.TSAcumulado = 0.0
+        self.CompletaronDemora = 0
 
-def main():
-    global mean_service
-    global mean_interarrival
-    global num_delays_required
-    global num_custs_delayed
-    global num_events
+        self.med_serv = med_serv
+        self.med_arrib = med_arrib
 
-    #Se establecen los parametros
-    num_events = 2
-    mean_service = 0.5
-    mean_interarrival = 1
-    num_delays_required = 1000
+    def inicializar(self):
+        self.Reloj = 0
+        self.TIempoUltimoEvento = 0
+        self.ProximoEvento = ""
+        self.EstadoServidor = "D"
+        self.TSAcumulado = 0
+        self.CompletaronDemora = 0
 
-    initialize()
+        # calcular primer arribo
+        self.ListaDeEventos.append(valorPoisson(self.med_arrib))
 
-    f = open("mm1.txt", "a")
-    f.write("\nParametros ingresados:")
-    f.write("\nTiempo promedio entre arribos (en minutos): " + str(mean_interarrival))
-    f.write("\nTiempo promedio de servicio (en minutos): " + str(mean_service))
-    f.write("\nNumero de clientes " + str(num_delays_required))
+        # print(self.ListaDeEventos)
 
-    while num_custs_delayed < num_delays_required:
-        timing()
-        update_time_avg_stats()
+        # se debe forzar a que el primer evento no sea una partida, ponemos bien alto el servicio
+        self.ListaDeEventos.append(99999.0)
+        self.Paso = 0
+        self.Iniciado = False
 
-        if(next_event_type == 1):
-            arrive()
+
+    def run(self):
+        
+        # llamo a la rutina de  inicializacion
+        self.inicializar()
+
+        # Repetir hasta que el reloj >= 50, nro clientes en cola = 0 y estadoservidor = "D"
+        while True:
+
+            #llamada a la rutina de tiempos
+            self.tiempos()
+
+            # llamada a la rutina correspondiente en funcion del tipo de evento
+            if(self.ProximoEvento == "ARRIBOS"):
+                self.arribos()
+
+
+
+    def tiempos(self):
+        self.TiempoUltimoEvento = self.Reloj
+        if(self.ListaDeEventos[0] <= self.ListaDeEventos[1]):
+            self.Reloj = self.ListaDeEventos[0]
+            self.ProximoEvento = "ARRIBOS"
         else:
-            depart()
-    report()
+            self.Reloj = self.ListaDeEventos[1]
+            self.ProximoEvento = "PARTIDAS"
 
-def initialize():
-    global time
-    global server_status
-    global num_in_q
-    global time_last_event
-    global num_custs_delayed
-    global total_of_delays
-    global area_num_in_q
-    global area_server_status
-    global time_next_event
-    global mean_interarrival
+    def arribos(self):
+        # un arribo genera otro arribo 
+        self.ListaDeEventos[0] = self.Reloj + valorPoisson(self.med_arrib)
 
-    #inicializar reloj
-    time = 0
+        # pregunto si el servidor esta desocupado
+        if self.EstadoServidor == "D":
+            # cambio el estado del servidor
+            self.EstadoServidor = "O"
 
-    #inicializar las variables de estado
-    server_status = IDLE
-    num_in_q = 0
-    time_last_event = 0
+            # programo el proximo evento de partida
+            self.ListaDeEventos[1] = self.Reloj + valorExponencial(self.med_serv)
 
-    #inicializo los contadores estadisticos
-    num_custs_delayed = 0
-    total_of_delays = 0
-    area_num_in_q = 0
-    area_server_status = 0
+            # acumulo el tiempo de servicio
+            self.TSAcumulado += (self.ListaDeEventos[1] - self.Reloj)
 
-    #inicializo lista de eventos
-    time_next_event[1] = time + expon(mean_interarrival)
-    time_next_event[2] = sys.maxsize
-
-    #crea archivo de registro
-    createFile()
+            # actualizo la cantidad de clientes que completaron demora
+            self.CompletaronDemora += 1
+        
+        else:
+            
 
 
-def timing():
-    global next_event_type
-    global num_events
-    global time_next_event
-    global time
 
-    min_time_next_event = 10**30 #por poner un numero exageradamente grande
-    next_event_type = 0
+def valorExponencial(media):
+    return np.random.exponential(1/media)
 
-    #determina el tipo del proximo evento
-    for i in range(num_events):
-        if (time_next_event[i+1] < min_time_next_event):
-            min_time_next_event = time_next_event[i+1]
-            next_event_type = i+1
+def valorPoisson(media):
+    return np.random.poisson(media)
 
-    #chequea si la lista de eventos esta vacia
-    if (next_event_type == 0):
-        #la lista de eventos esta vacia, por lo tanto termino la simulacion
-        print("Lista de eventos vacia en tiempo", time)
-    #la lista de eventos no esta vacia por lo que adelanto el reloj
-    time = min_time_next_event
+med_serv = float(input('Ingrese el tiempo medio de servicio por minuto: '))
+med_arrib = float(input('Ingrese el tiempo medio de arribos por minuto: '))
 
-def arrive():
-    global time_next_event
-    global time
-    global mean_interarrival
-    global server_status
-    global BUSY
-    global num_in_q
-    global Q_LIMIT
-    global total_of_delays
-    global num_custs_delayed
-
-    #programa el siguiente arrivo
-    time_next_event[1] = time + expon(mean_interarrival)
-
-    #valida si el servidor esta ocupado, entonces incrementa el numero de clientes en cola
-    if(server_status == BUSY):
-        #el servidor esta ocupado, entonces incrementa
-        num_in_q += 1
-
-        #valida si la cola supera el limite
-        if(num_in_q == Q_LIMIT):
-            f = open("mm1.txt", "a")
-            f.write("\nSe supero el limite de la cola en el tiempo " + str(time))
-            exit(2)
-        #aun hay espacio en la cola, por lo tanto se guarda el tiempo de arrivo del ultimo cliente al final  del arreglo
-        time_arrival[num_in_q] = time
-    else:
-        #el servidor está inactivo, por lo que el cliente que llega tiene un retraso de cero
-        #(Las siguientes dos declaraciones son para la claridad del programa y no afectará los resultados de la simulación)
-        delay = 0.0
-        total_of_delays += delay
-
-        #aumente el número de clientes retrasados ​​y hace que el servidor esté ocupado
-        num_custs_delayed += 1
-        server_status = BUSY
-
-        #finalizacion del servicio
-        time_next_event[2] = time + expon(mean_service)
-
-
-def depart():
-    global num_in_q
-    global IDLE
-    global server_status
-    global time_next_event
-    global time
-    global time_arrival
-    global num_custs_delayed
-    global mean_service
-    global total_of_delays
-
-    #Comprueba si la cola esta vacia
-    if(num_in_q == 0):
-        #la cola esta vacia, entonces el estado del servidor pasa a estado inactivo
-        server_status = IDLE
-        time_next_event[2] = sys.maxsize
-    else:
-        #la cola no esta vacia por lo tanto se reduce la cantidad de clientes en espera
-        num_in_q -= 1
-
-        #calcule el retraso del cliente que está comenzando el servicio y retraso total acumulado.
-        delay = time - time_arrival[1]
-        total_of_delays += delay
-
-        #incrementa el numero de clientes retrasados
-        num_custs_delayed += 1
-        time_next_event[2] = time + expon(mean_service)
-
-        #mueva a cada cliente en la cola un lugar
-        for i in range(num_in_q):
-            time_arrival[i] = time_arrival[i+1]
-
-
-def report():
-    global total_of_delays
-    global num_custs_delayed
-    global area_num_in_q
-    global time
-    global area_server_status
-
-    #calcular y escribie estimaciones del rendimiento en el registro.
-    f = open("mm1.txt", "a")
-    f.write("\nRetraso promedio en cola (en minutos): " + str(total_of_delays / num_custs_delayed))
-    f.write("\nCantidad promedio de personas en cola: " + str(area_num_in_q/time))
-    f.write("\nUtilizacion del servidor: " + str(area_server_status / time))
-    f.write("\nTiempo en que finalizo la simulacion (en minutos): " + str(time)+"\n")
-
-
-def update_time_avg_stats():
-    global time
-    global time_last_event
-    global area_num_in_q
-    global num_in_q
-    global area_server_status
-
-    #calcula el tiempo desde el ultimo evento y actualiza la marca last_event_time
-    time_since_last_event = time - time_last_event
-    time_last_event = time
-
-    area_num_in_q += num_in_q * time_since_last_event
-    area_server_status += server_status * time_since_last_event
-
-
-def expon(mean):
-    u = random.random()
-    return - mean * math.log(u, math.e)
-
-
-def createFile():
-    if os.path.exists("mm1.txt"):
-        os.remove("mm1.txt")
-    open("mm1.txt", "x")
-
-
-main()
-
+cola = Colas()
+cola.run()
--- TP3/TP3.0.py
+++ TP3/TP3.0.py
@@ -1,5 +1,12 @@
 import numpy as np
 from array import array
+import matplotlib.pyplot as plt
+
+l_nroCliCoProm_arr = []
+l_nroCliSisProm_arr = []
+l_utilPromServ_arr = []
+l_demPromCli_arr = []
+l_demPromCli_sist_arr = []
 
 
 class Colas(object):
@@ -17,9 +24,17 @@ class Colas(object):
         self.TiempoUltimoEvento = 0.0
         self.CompletaronDemora = 0
         self.Paso = 0
-        self.TMDeServicio = float(input('Ingrese el tiempo medio de servicio: '))
-        self.TMEntreArribos = (float(input('Ingrese el tiempo medio entre arribo con respecto al tiempo de servicio (porcentaje): ')) * self.TMDeServicio)      
+        self.TMDeServicio = 0.0
+        self.TMEntreArribos = 0.0      
         self.Iniciado = False
+        self.TMDeServicio = glob_TMDeServicio
+        self.TMEntreArribos = glob_TMEntreArribos
+
+        self.nroCliCoProm_arr = []
+        self.nroCliSisProm_arr = []
+        self.utilPromServ_arr = []
+        self.demPromCli_arr = []
+        self.demPromCli_sist_arr = []
 
     def inicializar(self):
         
@@ -34,7 +49,6 @@ class Colas(object):
         self.CompletaronDemora = 0
 
         # se debe calcular el tiempo del primer arribo
-
         self.ListaDeEventos.append(valorExponencial(self.TMEntreArribos))
         
         # Se debe forzar a que el primer evento no sea una partida, por lo tanto hay que asignarle un valor alto
@@ -43,22 +57,17 @@ class Colas(object):
         self.Iniciado = False
 
     # se define el algoritmo o subrutina principal
-
     def run(self):
 
         # llamo a la rutina de inicializacion
-
         self.inicializar()
 
         # Repetir hasta que el reloj >= 50, el nro de clientes en cola  = 0 y estadoservidor = "D"
-
         while True:
             # llamada a la rutina de tiempos
-
             self.tiempos()
 
             # llamada a la rutina correspondiente en funcion del tipo de evento que ocurre
-
             if (self.ProximoEvento == "ARRIBOS"):
                 self.arribos()
             else:
@@ -72,12 +81,11 @@ class Colas(object):
     def arribos(self):
 
         # un arribo genera otro arribo
-
         self.ListaDeEventos[0] = self.Reloj + valorExponencial(self.TMEntreArribos)
 
         # pregunto si el servidor esta desocupado
-
         if self.EstadoServidor == "D":
+
             # cambio el estado del servidor a ocupado
             self.EstadoServidor = "O"
 
@@ -88,10 +96,10 @@ class Colas(object):
             self.TSAcumulado += (self.ListaDeEventos[1] - self.Reloj)
 
             # actualizo la cantidad de clientes que completaron la demora
-
             self.CompletaronDemora += 1
         
         else:
+
             # calculo el area bajo Q(t) desde el momento actual del reloj hacia atras (TiempoUltimoEvento)
             self.AreaQDeT += (self.NroDeClientesEnCola * (self.Reloj - self.TiempoUltimoEvento))
 
@@ -104,9 +112,11 @@ class Colas(object):
 
     def partidas(self):
 
-        # pregunto si hay clientes en cola
 
+
+        # pregunto si hay clientes en cola
         if self.NroDeClientesEnCola > 0:
+
             # tiempo del proximo evento de partida
             self.ListaDeEventos[1] = self.Reloj + valorExponencial(self.TMDeServicio)
 
@@ -123,7 +133,6 @@ class Colas(object):
             self.AreaQDeT += (self.NroDeClientesEnCola * (self.Reloj - self.TiempoUltimoEvento))
 
             # decremento la cantidad de clientes en cola
-
             self.NroDeClientesEnCola -= 1
 
             # llamo a la rutina encargada de gestionar la cola, en este caso devera desplazar todos los valores una posicion hacia adelante
@@ -137,6 +146,8 @@ class Colas(object):
 
 
     def tiempos(self):
+
+        self.act_reportes()
         
         self.TiempoUltimoEvento = self.Reloj
         if (self.ListaDeEventos[0] <= self.ListaDeEventos[1]):
@@ -146,33 +157,248 @@ class Colas(object):
             self.Reloj = self.ListaDeEventos[1]
             self.ProximoEvento = "PARTIDAS"
 
-    def reportes(self):
+    def act_reportes(self):
+
+        # promedio de clientes en cola
         try:
             nroCliCoProm = self.AreaQDeT / self.Reloj
         except ZeroDivisionError:
             nroCliCoProm = 0
         
-        print("Nro promedio de clientes en cola: ", nroCliCoProm)
+        # print("Nro promedio de clientes en cola: ", nroCliCoProm)
+        self.nroCliCoProm_arr.append(nroCliCoProm)
 
+        # promedio de clientes en sistema
+        try:
+            nroCliSisProm = (self.AreaQDeT + self.TSAcumulado) / self.Reloj
+        except ZeroDivisionError:
+            nroCliSisProm = 0
+        
+        # print("Nro promedio de clientes en cola: ", nroCliCoProm)
+        self.nroCliSisProm_arr.append(nroCliSisProm)
+
+        # utilizacion del servidor
         try:
             utilPromServ = self.TSAcumulado / self.Reloj
         except ZeroDivisionError:
             utilPromServ = 0
         
-        print("Utilizacion promedio de los servidores: ", utilPromServ)
+        # print("Utilizacion promedio de los servidores: ", utilPromServ)
+        self.utilPromServ_arr.append(utilPromServ)
 
+        # promedio de tiempo en cola por cliente
         try:
             demPromCli = self.DemoraAcumulada / self.CompletaronDemora
         except ZeroDivisionError:
             demPromCli = 0
+
+        # print("Demora promedio de cliente: ", demPromCli)
+        self.demPromCli_arr.append(demPromCli)
         
-        print("Demora promedio de cliente: ", demPromCli)
+        # promedio de tiempo en el sistema
+        try:
+            demPromCli_sist = (self.DemoraAcumulada + self.TSAcumulado) / self.CompletaronDemora
+        except ZeroDivisionError:
+            demPromCli_sist = 0
+
+        # print("Demora promedio de cliente: ", demPromCli)
+        self.demPromCli_sist_arr.append(demPromCli_sist)
+    
+    def reportes(self):
+        l_nroCliCoProm_arr.append(self.nroCliCoProm_arr)
+        l_nroCliSisProm_arr.append(self.nroCliSisProm_arr)
+        l_utilPromServ_arr.append(self.utilPromServ_arr)
+        l_demPromCli_arr.append(self.demPromCli_arr)
+        l_demPromCli_sist_arr.append(self.demPromCli_sist_arr)
+
 
 def valorExponencial(media):
         return np.random.exponential(1/media)
 
+def ProbNClientes(MA, MS, n):
+    probN = (1 - (MA / MS)) * ((MA / MS) ** n)
+    return probN
+
+def getAxis(lista, med, multi):
+    std = np.std(lista)
+    mini = med - multi * std
+    maxi = med + multi * std
+    return mini, maxi
+
 # ejecucion del modelo de simulacion
 
-simulacion = Colas()
-simulacion.run()
     
+n_it = int(input("Ingrese numero de iteraciones: "))
+glob_TMDeServicio = (float(input('Ingrese el tiempo medio de servicio: ')) )
+glob_TMEntreArribos = (float(input('Ingrese el tiempo medio entre arribo: ')) )
+
+
+for i in range(n_it):
+    simulacion = Colas()
+    simulacion.run()
+
+# menu para seleccionar reporte
+
+while True:
+    print("\n")
+    print("\n")
+    print("Seleccione su opcion de reporte: ")
+    print("\n")
+    print("1 - Promedio de clientes en cola")
+    print("2 - Promedio de clientes en sistema")
+    print("3 - Utilizacion del servidor")
+    print("4 - Demora promedio en cola")
+    print("5 - Demora promedio en sistema")
+    print("6 - Probabilidad de n clientes en cola")
+    print("7 - Probabilidades de denegacion de servicio")
+    print("0 - Salir")
+    menu = int(input("Opcion seleccionada: "))
+
+    if menu == 1:
+        minis = []
+        maxis = []
+        longi = []
+        teo_arr = []
+        teo = ((glob_TMEntreArribos ** 2) / (glob_TMDeServicio * (glob_TMDeServicio - glob_TMEntreArribos)))
+
+        for i in range(n_it):
+            longi.append(len(l_nroCliCoProm_arr[i]))
+            mini, maxi = getAxis(l_nroCliCoProm_arr[i], teo, 3)
+            minis.append(mini)
+            maxis.append(maxi)
+            plt.plot(l_nroCliCoProm_arr[i])
+
+        for i in range(max(longi)):
+            teo_arr.append(teo)
+
+        min_gral = min(minis)   
+        max_gral = max(maxis)
+
+        plt.plot(teo_arr, color= "black", label= "Valor Teorico")
+        plt.legend(loc= "upper right")
+        plt.axis([0, min(longi), min_gral, max_gral])
+        plt.title("Promedio de clientes en cola") 
+        plt.show()         
+        
+    elif menu == 2:
+        minis = []
+        maxis = []
+        longi = []
+        teo_arr = []
+        teo = ((glob_TMEntreArribos ** 2) / (glob_TMDeServicio * (glob_TMDeServicio - glob_TMEntreArribos))) + (glob_TMEntreArribos / glob_TMDeServicio)
+
+        for i in range(n_it):
+            longi.append(len(l_nroCliCoProm_arr[i]))
+            mini, maxi = getAxis(l_nroCliCoProm_arr[i], teo, 3)
+            minis.append(mini)
+            maxis.append(maxi)
+            plt.plot(l_nroCliSisProm_arr[i])
+        
+        for i in range(max(longi)):
+            teo_arr.append(teo)
+
+        min_gral = min(minis)   
+        max_gral = max(maxis)
+
+        plt.plot(teo_arr, color= "black", label= "Valor Teorico")
+        plt.legend(loc= "upper right")
+        plt.axis([0, min(longi), min_gral, max_gral])    
+        plt.title("Promedio de clientes en el sistema") 
+        plt.show()
+
+    elif menu == 3:
+        minis = []
+        maxis = []
+        longi = []
+        util_teo_arr = []
+        util_teo = glob_TMEntreArribos / glob_TMDeServicio
+        
+        for i in range(n_it):
+            longi.append(len(l_utilPromServ_arr[i]))
+            mini, maxi = getAxis(l_utilPromServ_arr[i], util_teo, 2)
+            minis.append(mini)
+            maxis.append(maxi)
+            plt.plot(l_utilPromServ_arr[i])
+        for i in range(max(longi)):
+            util_teo_arr.append(util_teo)
+
+        min_gral = min(minis)   
+        max_gral = max(maxis)   
+
+        
+        plt.plot(util_teo_arr, color= "black", label= "Valor Teorico")
+        plt.legend(loc= "upper right")
+        plt.axis([0, min(longi), min_gral, max_gral])
+        plt.title("Utilizacion promedio del servidor") 
+        plt.show()
+
+    elif menu == 4:
+        minis = []
+        maxis = []
+        longi = []
+        teo_arr = []
+        teo = (glob_TMEntreArribos / (glob_TMDeServicio * (glob_TMDeServicio - glob_TMEntreArribos)))
+
+        for i in range(n_it):
+            longi.append(len(l_nroCliCoProm_arr[i]))
+            mini, maxi = getAxis(l_nroCliCoProm_arr[i], teo, 0.1)
+            minis.append(mini)
+            maxis.append(maxi)
+            plt.plot(l_demPromCli_arr[i])
+
+        for i in range(max(longi)):
+            teo_arr.append(teo)
+
+        min_gral = min(minis)   
+        max_gral = max(maxis)
+
+        plt.plot(teo_arr, color= "black", label= "Valor Teorico")
+        plt.legend(loc= "upper right")
+        plt.axis([0, min(longi), min_gral, max_gral])
+        plt.title("Promedio de demora en cola") 
+        plt.show()
+
+    elif menu == 5:
+        minis = []
+        maxis = []
+        longi = []
+        teo_arr = []
+        teo = (glob_TMEntreArribos / (glob_TMDeServicio * (glob_TMDeServicio - glob_TMEntreArribos))) + (1 / glob_TMDeServicio)
+
+
+        for i in range(n_it):
+            longi.append(len(l_nroCliCoProm_arr[i]))
+            mini, maxi = getAxis(l_nroCliCoProm_arr[i], teo, 0.1)
+            minis.append(mini)
+            maxis.append(maxi)
+            plt.plot(l_demPromCli_sist_arr[i])
+
+        
+        for i in range(max(longi)):
+            teo_arr.append(teo)
+
+        min_gral = min(minis)   
+        max_gral = max(maxis)
+
+        plt.plot(teo_arr, color= "black", label= "Valor Teorico")
+        plt.legend(loc= "upper right")
+        plt.axis([0, min(longi), min_gral, max_gral])
+        plt.title("Promedio de demora en el sistema") 
+        plt.show()
+
+    elif menu == 6:
+        n = int(input("Ingrese valor n para probabilidad de n en cola: "))
+
+        probN = ProbNClientes(glob_TMEntreArribos, glob_TMDeServicio, n)
+
+        print("La probabilidad de que hayan ", n , " o mas elementos en la cola es: ", probN)
+
+    elif menu == 7:
+        arr_den = [0, 2, 5, 10, 50]
+
+        for i in range(len(arr_den)):
+            probN = ProbNClientes(glob_TMEntreArribos, glob_TMDeServicio, arr_den[i])
+            print("La probabilidad de denegacion para una cola finita de tamaño ", arr_den[i] , " es: " , probN)
+
+    elif menu == 0:
+        break
